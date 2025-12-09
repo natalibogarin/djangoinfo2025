0990291e2e62371576b8b3ff49367364f488f86f
natalibogarin <natali.bogarin@gmail.com>
2025-12-08 23:28:47 -0300

    Agrego estructura inicial

--- proyectoInfo/settings.py
+++ proyectoInfo/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/nataliabogarin/static/'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/nataliabogarin/media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
